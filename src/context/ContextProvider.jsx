5ab08bde3e5a03baf72ebfc533a2c8c928f66b62
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-09-30 19:04:15 +0530

    Update Context

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -2,20 +2,38 @@ import { useContext, createContext, useState } from "react";
 
 export const StateContext = createContext();
 
+const initialState = {
+  chat: false,
+  notification: false,
+  cart: false,
+  userProfile: false,
+};
+
 export const useStateContext = () => {
   return useContext(StateContext);
 };
 
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
 
-  // const value = {
-  //   activeMenu,
-  //   setActiveMenu,
-  // };
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
 
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu }}>
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+      }}
+    >
       {children}
     </StateContext.Provider>
   );
