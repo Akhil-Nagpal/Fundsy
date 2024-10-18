5509a56ce7df03c845c1e2845150f4c3b14ff977
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-10-18 21:27:00 +0530

    Layout deployed from the code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 function App() {
   return (
     <>
-      <h1 className="text-32 text-cyan-500">Hello World</h1>
+      <div className="h-screen flex flex-col-reverse lg:flex-row ">
+        <div className="lg:w-300">Sidebar</div>
+        <div className="grow overflow-y-auto">
+          <div className="max-w-1140 mx-auto">Content</div>
+        </div>
+      </div>
     </>
   );
 }
